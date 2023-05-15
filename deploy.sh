7888e56988a64d66241a2add14a477f113994f35
bogdanssh <bshyshka@gmail.com>
2023-05-15 13:12:21 +0300

    Create deploy.sh
    initial deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn docs:build
+
+# navigate into the build output directory
+cd docs/.vuepress/dist
+
+# if you are deploying to a custom domain
+echo 'docsnew.cloudlinux.com' > CNAME
+
+git config --global user.email "bshyshka@gmail.com"
+git config --global user.name "circle-ci"
+
+git init
+git add -A
+git commit -m 'deploy [skip ci]'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:bogdanssh.github.io/cloudlinux-documentation.git master:gh-pages
+
+cd -
